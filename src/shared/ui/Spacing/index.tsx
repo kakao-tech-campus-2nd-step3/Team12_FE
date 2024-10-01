d944d164e127086e805fbde052f86e7209bdb947
mingkyeongg <ming0820@naver.com>
2024-10-02 00:06:33 +0900

    feat: Spacing 컴포넌트 가져오기

---
+++ src/shared/ui/Spacing/index.tsx
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+
+const vars = {
+  initial: '0',
+  xs: '520px',
+  sm: '768px',
+  md: '1024px',
+  lg: '1280px',
+};
+
+type ResponseGridStyle = {
+  [key in keyof typeof vars]?: number;
+};
+
+type Props = {
+  height?: number | ResponseGridStyle;
+  backgroundColor?: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function Spacing({ height, backgroundColor = 'inherit', ...props }: Props) {
+  return <Wrapper height={height} backgroundColor={backgroundColor} {...props} />;
+}
+
+const Wrapper = styled.div<Pick<Props, 'height' | 'backgroundColor'>>(
+  {
+    width: '100%',
+  },
+  ({ backgroundColor }: { backgroundColor: string }) => ({ backgroundColor }),
+  ({ height = 16 }) => {
+    if (typeof height === 'number') {
+      return {
+        height: `${height}px`,
+      };
+    }
+
+    const breakpoints = Object.keys(height) as (keyof typeof vars)[];
+    return breakpoints
+      .map((breakpoint) => `@media screen and (min-width: ${vars[breakpoint]}) { height: ${height[breakpoint]}px; }`)
+      .join(' ');
+  },
+);
