329a70f2c6879d5fc28e7f1a90587b15e3a38b19
harugi7 <gimme2721@gmail.com>
2024-10-01 19:08:23 +0900

    chore: 폴더 구조 수정

---
+++ src/constants/breakpoints.tsx
@@ -0,0 +1,14 @@
+export const BREAKPOINT_XS = '520px';
+
+export const BREAKPOINT_SM = '768px';
+
+export const BREAKPOINT_MD = '1024px';
+
+export const BREAKPOINT_LG = '1280px';
+
+export const breakPoints = {
+  xs: BREAKPOINT_XS,
+  sm: BREAKPOINT_SM,
+  md: BREAKPOINT_MD,
+  lg: BREAKPOINT_LG,
+};
