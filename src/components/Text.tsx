9f0d79dec7d6c2e08d3c1fa6216110c7c6028019
harugi7 <gimme2721@gmail.com>
2024-10-02 13:51:46 +0900

    feat: ESLint에 맞춰 CamelCase로 변환

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -4,7 +4,7 @@ import styled from '@emotion/styled';
 export type FontWeight = 'regular' | 'medium' | 'bold' | 'lighter' | 'bolder';
 
 export interface TextProps {
-  children?: ReactNode;
+  children: ReactNode;
   weight?: FontWeight;
   as?: ElementType;
   fontSize?: string;
@@ -17,17 +17,17 @@ const StyledText = styled.p<TextProps>`
 `;
 
 export const Paragraph = {
-  large: ({ children, weight }: TextProps) => (
+  Large: ({ children, weight }: TextProps) => (
     <StyledText as="p" fontSize="18px" weight={weight}>
       {children}
     </StyledText>
   ),
-  medium: ({ children, weight }: TextProps) => (
+  Medium: ({ children, weight }: TextProps) => (
     <StyledText as="p" fontSize="16px" weight={weight}>
       {children}
     </StyledText>
   ),
-  small: ({ children, weight }: TextProps) => (
+  Small: ({ children, weight }: TextProps) => (
     <StyledText as="p" fontSize="14px" weight={weight}>
       {children}
     </StyledText>
@@ -35,33 +35,28 @@ export const Paragraph = {
 };
 
 export const Heading = {
-  h1: ({ children, weight }: TextProps) => (
+  H1: ({ children, weight }: TextProps) => (
     <StyledText as="h1" fontSize="32px" weight={weight}>
       {children}
     </StyledText>
   ),
-  h2: ({ children, weight }: TextProps) => (
+  H2: ({ children, weight }: TextProps) => (
     <StyledText as="h2" fontSize="28px" weight={weight}>
       {children}
     </StyledText>
   ),
-  h3: ({ children, weight }: TextProps) => (
+  H3: ({ children, weight }: TextProps) => (
     <StyledText as="h3" fontSize="24px" weight={weight}>
       {children}
     </StyledText>
   ),
-  h4: ({ children, weight }: TextProps) => (
+  H4: ({ children, weight }: TextProps) => (
     <StyledText as="h4" fontSize="20px" weight={weight}>
       {children}
     </StyledText>
   ),
-  h5: ({ children, weight }: TextProps) => (
+  H5: ({ children, weight }: TextProps) => (
     <StyledText as="h5" fontSize="18px" weight={weight}>
-      {children}
-    </StyledText>
-  ),
-  h6: ({ children, weight }: TextProps) => (
-    <StyledText as="h6" fontSize="16px" weight={weight}>
       {children}
     </StyledText>
   ),
