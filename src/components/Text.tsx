84cf5fa3afe51054e3ad6a38c7d3d03ef46fad54
harugi7 <132315935+harugi7@users.noreply.github.com>
2024-10-03 13:23:17 +0900

    week5/harugi7 (#14)
    * chore: install emotion
    
    * chore: 폴더 구조 수정
    
    * feat: ESLint에 맞춰 CamelCase로 변환
    
    * feat: Container 컴포넌트 구현

---
+++ src/components/Text.tsx
@@ -0,0 +1,63 @@
+import { ElementType, ReactNode } from 'react';
+import styled from '@emotion/styled';
+
+export type FontWeight = 'regular' | 'medium' | 'bold' | 'lighter' | 'bolder';
+
+export interface TextProps {
+  children: ReactNode;
+  weight?: FontWeight;
+  as?: ElementType;
+  fontSize?: string;
+}
+
+const StyledText = styled.p<TextProps>`
+    font-weight: ${({ weight }) => weight || 'regular'};
+    margin: 0;
+    ${({ fontSize }) => fontSize && `font-size: ${fontSize};`}
+`;
+
+export const Paragraph = {
+  Large: ({ children, weight }: TextProps) => (
+    <StyledText as="p" fontSize="18px" weight={weight}>
+      {children}
+    </StyledText>
+  ),
+  Medium: ({ children, weight }: TextProps) => (
+    <StyledText as="p" fontSize="16px" weight={weight}>
+      {children}
+    </StyledText>
+  ),
+  Small: ({ children, weight }: TextProps) => (
+    <StyledText as="p" fontSize="14px" weight={weight}>
+      {children}
+    </StyledText>
+  ),
+};
+
+export const Heading = {
+  H1: ({ children, weight }: TextProps) => (
+    <StyledText as="h1" fontSize="32px" weight={weight}>
+      {children}
+    </StyledText>
+  ),
+  H2: ({ children, weight }: TextProps) => (
+    <StyledText as="h2" fontSize="28px" weight={weight}>
+      {children}
+    </StyledText>
+  ),
+  H3: ({ children, weight }: TextProps) => (
+    <StyledText as="h3" fontSize="24px" weight={weight}>
+      {children}
+    </StyledText>
+  ),
+  H4: ({ children, weight }: TextProps) => (
+    <StyledText as="h4" fontSize="20px" weight={weight}>
+      {children}
+    </StyledText>
+  ),
+  H5: ({ children, weight }: TextProps) => (
+    <StyledText as="h5" fontSize="18px" weight={weight}>
+      {children}
+    </StyledText>
+  ),
+};
