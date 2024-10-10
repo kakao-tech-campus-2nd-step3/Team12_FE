a51a50efc635f41b58342b16561d7319732752c5
mingkyeongg <ming0820@naver.com>
2024-10-10 17:35:31 +0900

    feat: Login 창 요소 추가

--- src/features/login/index.tsx
+++ src/features/login/index.tsx
@@ -1,5 +1,9 @@
 import { DefaultPaddedContainer } from "@/components/container/variants";
 import Modal from "@/components/modal";
+import Logo from "@/assets/Logo.svg"
+import { Paragraph } from "@/components/text";
+import Button from "@/components/button";
+import Grid from "@/components/grid";
 
 interface LoginModalProps {
   open: boolean;
@@ -10,8 +14,15 @@ function LoginModal({open, onClose}: LoginModalProps) {
 	return (
 		<Modal open={open} onClose={onClose}>
 			<DefaultPaddedContainer>
-				<h1>로그인</h1>
-				<p>로그인 내용...</p>
+				<img src={Logo} alt="Logo" />
+				<Paragraph.Medium>함께하면 더 강해진다! <br /> Ditto와 함께 시작하세요.</Paragraph.Medium>
+				<Paragraph.Small>sns로 5초만에 시작하기</Paragraph.Small>
+				<Button variant="default">카카오 계정으로 로그인</Button>
+				<Grid columns={3}>
+					<Button variant="default">깃허브</Button>
+					<Button variant="default">인스타</Button>
+					<Button variant="default">블로그</Button>
+				</Grid>
 			</DefaultPaddedContainer>
 		</Modal>
 	);
