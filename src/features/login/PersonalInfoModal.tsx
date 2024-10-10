89ac376860c98a6b9e10fcc83325d508721fd54a
mingkyeongg <ming0820@naver.com>
2024-10-10 20:38:37 +0900

    feat: PersonalInfoModal 요소 삽입

--- src/features/login/PersonalInfoModal.tsx
+++ src/features/login/PersonalInfoModal.tsx
@@ -1,16 +1,47 @@
+import Avatar from "@/components/avatar";
+import Button from "@/components/button";
+import Checkbox from "@/components/checkbox";
+import Container from "@/components/container";
 import { DefaultPaddedContainer } from "@/components/container/variants";
+import Grid from "@/components/grid";
+import Input from "@/components/input";
 import Modal from "@/components/modal";
+import Spacing from "@/components/spacing";
+import { Heading, Paragraph } from "@/components/text";
+import TextArea from "@/components/textarea";
 
 interface PersonalInfoModalProps {
   open: boolean;
   onClose: () => void;
 }
 
-export function personalInfoModal({ open, onClose }: PersonalInfoModalProps) {
+export function PersonalInfoModal({ open, onClose }: PersonalInfoModalProps) {
   return (
     <DefaultPaddedContainer>
       <Modal open={open} onClose={onClose} width="447px" height="697px">
-        <h1>PersonalInfoModal</h1>
+        <Container padding="30px" direction="column" align="flex-start">
+          <Heading.H3 weight="bold">개인 정보를 입력해주세요.</Heading.H3>
+        </Container>
+
+        <Container direction="column">
+          <Avatar size="large" />
+          <Button>프로필 사진 등록</Button>
+        </Container>
+
+        <Container>
+          <Grid columns={1}>
+            <Input label="닉네임" placeholder="디토에서 사용할 닉네임이에요." type={"text"} />
+            <Input label="이메일" placeholder="이메일 주소를 입력해주세요." type={"email"} />
+            <Input label="연락처" placeholder="연락 가능한 번호를 입력해주세요." type={"tel"} />
+            <TextArea label="자기소개" />
+          </Grid>
+        </Container>
+
+        <Container>
+          <Checkbox />
+          <Paragraph.Small>개인정보 처리방침 및 이용약관에 동의해요.</Paragraph.Small>
+        </Container>
+        <Button>가입하기</Button>
       </Modal>
     </DefaultPaddedContainer>
   );
--- src/providers/BrowserRouterProvider.tsx
+++ src/providers/BrowserRouterProvider.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { PersonalInfoModal } from '../features/login/PersonalInfoModal';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: (
-      <div>Hello World</div>
+      <>
+        <div>Hello World</div> 
+        <PersonalInfoModal open={true} onClose={() => {}}/>
+      </>
     ),
   },
 ]);
