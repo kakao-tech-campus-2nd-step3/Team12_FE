ee0babc485280472cad6cf0bcee92688ed2b0891
Cla6Shade <111969754+cla6shade@users.noreply.github.com>
2024-10-13 14:52:14 +0900

    feat: 일부 컴포넌트 변경 및 form validation 구현 (#31)
    * feat: error message for the form input
    
    * style: 에러메시지가 padding을 가지도록 수정
    
    * feat: 스터디 생성 관련 타입 정의
    
    * fix: onChange가 제대로 전달되지 않던 문제점 해결
    
    * feat: 스터디 생성 시 form validation을 수행하도록 수정
    
    * Feedback/6th week (#29)
    
    * feat: avatar 컴포넌트 border 속성 추가
    
    * refactor: avatar 스타일 삭제
    
    * refactor: 모달창 defaultpadding 컴포넌트 삭제
    
    * refactor: ModalContainer 기본 padding 추가
    
    * fix: onChange가 제대로 전달되지 않던 문제점 해결 (#30)
    
    * feat: error message for the form input
    
    * style: 에러메시지가 padding을 가지도록 수정
    
    * feat: 스터디 생성 관련 타입 정의
    
    * feat: 스터디 생성 시 form validation을 수행하도록 수정
    
    * style: 버튼 스타일 수정
    
    * test: 변경된 버튼 variant 반영
    
    * refactor: 버튼 스타일링을 재정의하는 대신 variant를 사용하도록 변경
    
    * refactor: 버튼 스타일링을 재정의하는 대신 variant를 사용하도록 변경
    
    * refactor: 사용되지 않는 import statement 제거
    
    ---------
    
    Co-authored-by: Lee Minkyeong <122078277+mingkyeongg@users.noreply.github.com>

--- src/features/modal/studyCreation/StudyCreationModal.tsx
+++ src/features/modal/studyCreation/StudyCreationModal.tsx
@@ -1,7 +1,9 @@
-import Container from '@/components/container';
+import { css } from '@emotion/react';
+import { FormState, useForm, UseFormRegister } from 'react-hook-form';
 import Modal from '@/components/modal';
 import LeftSection from '@/features/modal/studyCreation/LeftSection';
 import RightSection from '@/features/modal/studyCreation/RightSection';
+import { type StudyCreationInputs } from '@/types/study';
 
 interface StudyCreationProps {
   open: boolean;
@@ -9,12 +11,40 @@ interface StudyCreationProps {
 }
 
 export default function StudyCreationModal({ open, onClose }: StudyCreationProps) {
+  const {
+    register,
+    handleSubmit,
+    formState,
+  } = useForm<StudyCreationInputs>({
+    defaultValues: {
+      isOpen: true,
+      name: '',
+      topic: '',
+      description: '',
+    },
+    mode: 'onChange',
+  });
+
+  // TODO: 실제 request 로직으로 변경 및 UI 로직과 분리
+  const onSubmit = (data: StudyCreationInputs) => console.log(data);
+
   return (
-    <Modal open={open} onClose={onClose} width="850px" height="450px">
-      <Container padding="30px" gap="30px" align="flex-start">
-        <LeftSection />
-        <RightSection />
-      </Container>
+    <Modal open={open} onClose={onClose} width="850px">
+      <form css={formStyle} onSubmit={handleSubmit(onSubmit)}>
+        <LeftSection formState={formState} register={register} />
+        <RightSection formState={formState} register={register} />
+      </form>
     </Modal>
   );
+}
+
+const formStyle = css`
+  display: flex;
+  gap: 30px;
+  padding: 30px;
+`;
+
+export interface StudyCreationSectionProps {
+  register: UseFormRegister<StudyCreationInputs>;
+  formState: FormState<StudyCreationInputs>;
 }
