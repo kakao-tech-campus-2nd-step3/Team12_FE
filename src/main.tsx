564dd77388bd7023bf3d48e4aa3cb5ad96913e92
harugi7 <gimme2721@gmail.com>
2024-10-14 20:02:23 +0900

    chore: main 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import BrowserRouterProvider from '@providers/BrowserRouterProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@emotion/react';
 import defaultTheme from '@styles/theme';
-import Header from '@components/header';
 import 'reset-css/reset.css';
 
 const queryClient = new QueryClient();
@@ -14,7 +13,6 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={defaultTheme}>
         <BrowserRouterProvider />
-        <Header />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
