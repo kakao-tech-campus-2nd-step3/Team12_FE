5df4c20ead403662abfca1d903268645f3cc05a2
harugi7 <gimme2721@gmail.com>
2024-10-14 19:50:58 +0900

    feat: banner 안의 ranking과 icebreaking

---
+++ src/components/ranking/index.tsx
@@ -0,0 +1,19 @@
+import Container from '@components/container';
+import { css } from '@emotion/react';
+
+function Ranking() {
+  return (
+    <Container
+      width="60%"
+      height="250px"
+      cssOverride={css`
+        background-color: bisque;
+        border-radius: 10px;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+      `}
+    >
+      Ranking
+    </Container>
+  );
+}
+export default Ranking;
