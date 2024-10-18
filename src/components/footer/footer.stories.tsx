8f9bccda15463c24f7f5c0c2bc4f8b9d1f9ad256
cla6shade <cla6shade@gmail.com>
2024-10-18 15:46:39 +0900

    refactor: 컨벤션에 맞춰 파일 확장자 제거

--- src/components/footer/footer.stories.tsx
+++ src/components/footer/footer.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Footer from "@components/footer/index.tsx";
+import Footer from "@components/footer";
 
 const meta: Meta<typeof Footer> = {
     title: 'Components/Footer',
