908d0c64721097780993a9cda0a0ce44058afa95
cla6shade <cla6shade@gmail.com>
2024-10-27 00:31:50 +0900

    test: 스터디 목록에 이름과 설명이 매우 긴 스터디 추가

--- src/mock/study/index.ts
+++ src/mock/study/index.ts
@@ -2,10 +2,10 @@ export const mockStudyList = [
   {
     id: 1,
     isOpen: false,
-    name: '스터디',
+    name: '스터디 이름인데 이제 매우 길이가 긴',
     createdAt: new Date(),
     topic: 'topic',
-    description: '스터디 설명 123',
+    description: '스터디 설명인데 엄청나게 긴 설명을 곁들인, 나야 스터디 스터디 설명인데 엄청나게 긴 설명을 곁들인, 나야 스터디 스터디 설명인데 엄청나게 긴 설명을 곁들인, 나야 스터디',
   },
   {
     id: 2,
