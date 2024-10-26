1e8f3149448ac5ce31c45b6cd366439a457e83cb
cla6shade <cla6shade@gmail.com>
2024-10-27 00:31:05 +0900

    feat: text 컴포넌트가 html attribute를 추가로 받을 수 있도록 변경

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,18 +1,14 @@
-import { ElementType, ReactNode } from 'react';
+import { ElementType, HTMLAttributes, ReactNode } from 'react';
 import styled from '@emotion/styled';
 import { useTheme } from '@emotion/react';
 
 export type FontWeight = 'regular' | 'medium' | 'bold' | 'lighter' | 'bolder';
 
-interface TextProps {
+interface TextProps extends HTMLAttributes<HTMLHeadingElement | HTMLParagraphElement> {
   children: ReactNode;
-  // eslint-disable-next-line react/no-unused-prop-types
   weight?: FontWeight;
-  // eslint-disable-next-line react/no-unused-prop-types
   as?: ElementType;
-  // eslint-disable-next-line react/no-unused-prop-types
   fontSize?: string;
-  // eslint-disable-next-line react/no-unused-prop-types
   color?: string;
 }
 
@@ -29,44 +25,46 @@ const Text = styled.p<TextProps>`
 
 export default Text;
 
-export function ErrorText({ children, variant = 'medium' }: VariantTextProps) {
+export function ErrorText({ children, variant = 'medium', ...rest }: VariantTextProps) {
   const theme = useTheme();
   const textSizes = {
     large: '16px', medium: '14px', small: '12px',
   };
-  return <Text as="p" fontSize={textSizes[variant]} color={theme.colors.other.error}>{children}</Text>;
+  return <Text as="p" fontSize={textSizes[variant]} color={theme.colors.other.error} {...rest}>{children}</Text>;
 }
 
-export function Paragraph({ children, variant = 'medium', weight }: VariantTextProps) {
+export function Paragraph({
+  children, variant = 'medium', weight, color, ...rest
+}: VariantTextProps) {
   const textSizes = {
     large: '18px', medium: '16px', small: '14px',
   };
-  return <Text as="p" fontSize={textSizes[variant]} weight={weight}>{children}</Text>;
+  return <Text as="p" fontSize={textSizes[variant]} weight={weight} color={color} {...rest}>{children}</Text>;
 }
 
 export const Heading = {
-  H1: ({ children, weight }: TextProps) => (
-    <Text as="h1" fontSize="32px" weight={weight}>
+  H1: ({ children, weight, ...rest }: TextProps) => (
+    <Text as="h1" fontSize="32px" weight={weight} {...rest}>
       {children}
     </Text>
   ),
-  H2: ({ children, weight }: TextProps) => (
-    <Text as="h2" fontSize="28px" weight={weight}>
+  H2: ({ children, weight, ...rest }: TextProps) => (
+    <Text as="h2" fontSize="28px" weight={weight} {...rest}>
       {children}
     </Text>
   ),
-  H3: ({ children, weight }: TextProps) => (
-    <Text as="h3" fontSize="24px" weight={weight}>
+  H3: ({ children, weight, ...rest }: TextProps) => (
+    <Text as="h3" fontSize="24px" weight={weight} {...rest}>
       {children}
     </Text>
   ),
-  H4: ({ children, weight }: TextProps) => (
-    <Text as="h4" fontSize="20px" weight={weight}>
+  H4: ({ children, weight, ...rest }: TextProps) => (
+    <Text as="h4" fontSize="20px" weight={weight} {...rest}>
       {children}
     </Text>
   ),
-  H5: ({ children, weight }: TextProps) => (
-    <Text as="h5" fontSize="18px" weight={weight}>
+  H5: ({ children, weight, ...rest }: TextProps) => (
+    <Text as="h5" fontSize="18px" weight={weight} {...rest}>
       {children}
     </Text>
   ),
