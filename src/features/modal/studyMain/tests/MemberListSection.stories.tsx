0e2a4c40a805c4ac5099fb115a7a57316a346352
harugi7 <gimme2721@gmail.com>
2024-11-06 15:03:07 +0900

    chore: storybook 제작

---
+++ src/features/modal/studyMain/tests/MemberListSection.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import MemberListSection from "@features/modal/studyMain/MemberListSection.tsx";
+
+const meta: Meta<typeof MemberListSection> = {
+    title: 'features/StudyMain/MemberListSection',
+    component: MemberListSection,
+    parameters: {
+        layout: 'fullscreen',
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof MemberListSection>;
+
+export const Default: Story = {};
