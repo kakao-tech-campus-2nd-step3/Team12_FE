9eb9500b5d950f4589f8e5c44c786b190f40bec7
cla6shade <cla6shade@github.com>
2024-11-06 17:18:21 +0900

    design: 변경 배너 디자인 반영

--- src/features/main/banner/index.tsx
+++ src/features/main/banner/index.tsx
@@ -1,19 +1,67 @@
-import Ranking from 'features/main/ranking';
-import IceBreaking from 'features/main/iceBreaking';
-import { DefaultPaddedContainer } from '@components/container/variants';
 import Container from '@components/container';
+import bannerImage from '@assets/banner-image.svg';
+import bannerBackground from '@assets/banner-background.webp';
+import logo from '@assets/logo-inset.svg';
+import { useTheme } from '@emotion/react';
+import { Paragraph } from '@components/text';
+import { PrimarySpan } from '@components/span';
+import Button from '@components/button';
+import { rootWidth } from '@styles/length';
 
 function Banner() {
   return (
-    <DefaultPaddedContainer>
-      <Container
-        padding="50px 0"
-        gap="30px"
+    <Container
+      height="480px"
+      cssOverride={{
+        backgroundImage: `url("${bannerBackground}")`,
+        backgroundSize: 'cover',
+        position: 'relative',
+        backgroundPosition: 'center right',
+      }}
+    >
+      <Container cssOverride={{
+        position: 'absolute',
+        background: 'rgba(0,0,0,0.6)',
+        zIndex: 3,
+        height: '480px',
+      }}
       >
-        <Ranking />
-        <IceBreaking />
+        <Container
+          justify="flex-start"
+          height="400px"
+          cssOverride={{
+            maxWidth: rootWidth,
+            position: 'absolute',
+          }}
+        >
+          <LeftSection />
+          <img src={bannerImage} alt="banner-image" css={{ width: '300px', paddingRight: '150px' }} />
+        </Container>
       </Container>
-    </DefaultPaddedContainer>
+    </Container>
   );
 }
+
+function LeftSection() {
+  const theme = useTheme();
+  return (
+    <Container padding="0 0 0 200px" align="flex-start" direction="column">
+      <h1 css={{ fontSize: '30px', fontWeight: 'bold', color: theme.colors.absolute.white }}>
+        함께하는
+        {' '}
+        <PrimarySpan>경쟁</PrimarySpan>
+      </h1>
+      <img css={{ width: '180px', marginBottom: '8px' }} src={logo} alt="banner-logo" />
+      <Paragraph variant="medium" color={theme.colors.absolute.white}>
+        매번 파토나는 비효율적인 스터디, 질리셨나요?
+      </Paragraph>
+      <Paragraph variant="medium" color={theme.colors.absolute.white}>
+        <PrimarySpan>함께 경쟁하며 성장하는 Ditto</PrimarySpan>
+        로 시작해보세요
+      </Paragraph>
+      <Button css={{ marginTop: '16px' }} variant="primary">5초만에 시작하기</Button>
+    </Container>
+  );
+}
+
 export default Banner;
