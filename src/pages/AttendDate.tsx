01219a89a1d459c125a5ae8ba86164272203bff6
mingkyeongg <ming0820@naver.com>
2024-11-06 23:07:22 +0900

    AttendDate 페이지 생성

---
+++ src/pages/AttendDate.tsx
@@ -0,0 +1,9 @@
+import Page from "@/components/template/Page";
+
+export default function AttendDate() {
+	return (
+    <Page>
+      <h1>AttendDate</h1>
+    </Page>
+	);
+}
--- src/providers/BrowserRouterProvider.tsx
+++ src/providers/BrowserRouterProvider.tsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainPage from '@pages/MainPage';
+import AttendDate from '@pages/AttendDate';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: (
       <MainPage />
+    ),
+  },
+  {
+    path: '/attend',
+    element: (
+      <AttendDate />
     ),
   },
 ]);
