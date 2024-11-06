14bb3547355b6ff68b0952ac2b3c2a257b1fded1
harugi7 <gimme2721@gmail.com>
2024-11-06 15:02:03 +0900

    chore: 스터디원 목데이터 추가

---
+++ src/mock/member/index.ts
@@ -0,0 +1,44 @@
+export const mockMemberList = [
+  {
+    nickname: '김디토',
+    email: 'agile@gmail.com',
+    introduction: '안녕하세용 김디토이고 하 뭐라해야하냐 목데이턴지 몫데이턴지 만드는게 제일 힘드네',
+    phone: '010-1234-1234',
+    agreeToTerms: true,
+  },
+  {
+    nickname: '김기토',
+    email: 'agile@gmail.com',
+    introduction: '안녕하세용 김디토이고 하 뭐라해야하냐 목데이턴지 몫데이턴지 만드는게 제일 힘드네',
+    phone: '010-1234-4321',
+    agreeToTerms: true,
+  },
+  {
+    nickname: '김니토',
+    email: 'agile@gmail.com',
+    introduction: '안녕하세용 김디토이고 하 뭐라해야하냐 목데이턴지 몫데이턴지 만드는게 제일 힘드네',
+    phone: '010-1234-2413',
+    agreeToTerms: false,
+  },
+  {
+    nickname: '김디토',
+    email: 'agile@gmail.com',
+    introduction: '안녕하세용 김디토이고 하 뭐라해야하냐 목데이턴지 몫데이턴지 만드는게 제일 힘드네 저는 동명방구입니다',
+    phone: '010-1234-3412',
+    agreeToTerms: true,
+  },
+  {
+    nickname: '김리토',
+    email: 'agile@gmail.com',
+    introduction: '안녕하세용 김디토이고 하 뭐라해야하냐 목데이턴지 몫데이턴지 만드는게 제일 힘드네저는 동명이인입니다',
+    phone: '010-1234-4231',
+    agreeToTerms: true,
+  },
+  {
+    nickname: '김미토',
+    email: 'agile@gmail.com',
+    introduction: '안녕하세용 김디토이고 하 뭐라해야하냐 목데이턴지 몫데이턴지 만드는게 제일 힘드네저는공명입니다.',
+    phone: '010-1234-1324',
+    agreeToTerms: false,
+  },
+];
