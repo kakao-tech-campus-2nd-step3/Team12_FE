42150e3fa2c6c98281f2df605e97d87138dfe25c
cla6shade <cla6shade@github.com>
2024-11-10 15:40:48 +0900

    feat: 개인정보 등록 메소드 구현

--- src/api/member/index.ts
+++ src/api/member/index.ts
@@ -1,8 +1,12 @@
 import endpoints from '@constants/endpoints';
-import { MyInfoResponse } from '@/types/member';
+import { MyInfoResponse, PersonalInfoInputs } from '@/types/member';
 import axiosInstance from '@/utils/network';
 
 export async function getMyInfo(): Promise<MyInfoResponse> {
   const response = await axiosInstance.get<MyInfoResponse>(endpoints.myInfo);
   return response.data;
+}
+
+export async function submitPersonalInfo(input: PersonalInfoInputs) {
+  await axiosInstance.post(endpoints.submitPersonalInfo, input);
 }
--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -4,6 +4,7 @@ const endpoints = {
   myInfo: `${prefix}/users`,
   reIssue: `${prefix}/reissue`,
   searchStudy: `${prefix}/studies`,
+  submitPersonalInfo: `${prefix}/auth`,
 };
 
 export default endpoints;
