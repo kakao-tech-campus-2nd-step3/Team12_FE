79b9fde2c61d20156432319b1bc4c1f29dd4bc48
harugi7 <gimme2721@gmail.com>
2024-11-11 14:42:12 +0900

    feat: notices endpoint 등록

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -4,6 +4,7 @@ const endpoints = {
   myInfo: `${prefix}/users`,
   reIssue: `${prefix}/reissue`,
   searchStudy: `${prefix}/studies`,
+  notices: `${prefix}/notices`,
 };
 
 export default endpoints;
