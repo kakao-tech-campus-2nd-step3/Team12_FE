d904a50a0b77303c603f81c2a52ef0c1a771224b
cla6shade <cla6shade@github.com>
2024-11-12 14:13:37 +0900

    feat: 스터디 멤버 관련 타입 정의

--- src/types/study/index.d.ts
+++ src/types/study/index.d.ts
@@ -1,3 +1,5 @@
+import type { Member } from '@/types/member';
+
 export interface Study {
   id: number;
   name: string;
@@ -9,6 +11,7 @@ export interface Study {
 }
 
 export type StudyFilter = 'all' | 'open' | 'closed';
+export type StudyRole = '스터디장' | '스터디원';
 
 // TODO: 추후에 profileImage input 구현 후 타입 변경
 // export type StudyCreationRequestBody =
@@ -32,3 +35,15 @@ export interface StudySearchResponse {
   maxPage: number;
   totalItemCount: number;
 }
+
+export type StudyInfoResponse = Study;
+
+export type StudyMember = {
+  member: Pick<Member, 'id' | 'nickname' | 'description' | 'profileImage'>;
+  role: StudyRole;
+  joined_at: Date;
+};
+
+export type StudyMembersResponse = StudyMember[];
+
+export type StudyInfoWithMembers = Study & { members: StudyMember[] };
