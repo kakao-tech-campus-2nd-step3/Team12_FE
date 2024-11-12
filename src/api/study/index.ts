d7d068b80d13bae6759abddbda12e046dc3bde98
cla6shade <cla6shade@github.com>
2024-11-12 14:13:14 +0900

    feat: study info api method

--- src/api/study/index.ts
+++ src/api/study/index.ts
@@ -1,10 +1,24 @@
 import endpoints from '@constants/endpoints';
-import { StudySearchRequestQuery, StudySearchResponse } from '@/types/study';
+import type {
+  StudyInfoResponse, StudyMembersResponse,
+  StudySearchRequestQuery,
+  StudySearchResponse,
+} from '@/types/study';
 import axiosInstance from '@/utils/network';
 
 export async function searchStudies(requestQuery: StudySearchRequestQuery) {
   const response = await axiosInstance.get<StudySearchResponse>(endpoints.searchStudy, {
     params: requestQuery,
   });
+  return response.data;
+}
+
+export async function getStudyInfo(studyId: number) {
+  const response = await axiosInstance.get<StudyInfoResponse>(endpoints.studyInfo(studyId));
+  return response.data;
+}
+
+export async function getStudyMembers(studyId: number) {
+  const response = await axiosInstance.get<StudyMembersResponse>(endpoints.studyMembers(studyId));
   return response.data;
 }
