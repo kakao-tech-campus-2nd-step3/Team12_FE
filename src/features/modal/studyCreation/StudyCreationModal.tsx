b4656328fcafeb44f732adff54eccfa83aa90a5c
cla6shade <cla6shade@github.com>
2024-11-14 11:08:25 +0900

    feat: default background 추가

--- src/features/modal/studyCreation/StudyCreationModal.tsx
+++ src/features/modal/studyCreation/StudyCreationModal.tsx
@@ -3,6 +3,7 @@ import {
   Control, FormState, useForm, UseFormRegister,
   UseFormSetValue,
 } from 'react-hook-form';
+import defaultBackground from '@assets/banner-background.webp';
 import Modal from '@/components/modal';
 import LeftSection from '@/features/modal/studyCreation/LeftSection';
 import RightSection from '@/features/modal/studyCreation/RightSection';
@@ -27,23 +28,29 @@ export default function StudyCreationModal({ open, onClose }: StudyCreationProps
       name: '',
       topic: '',
       description: '',
-      profileImage: '',
     },
     mode: 'onChange',
   });
 
   // TODO: 실제 request 로직으로 변경 및 UI 로직과 분리
-  function onSubmit(data: StudyCreationInputs) {
+  async function onSubmit(data: StudyCreationInputs) {
     const formData = new FormData();
 
     const requestData = {
       name: data.name,
       description: data.description,
       topic: data.topic,
-      isOpen: data.is_open,
+      is_open: data.is_open,
     };
-    formData.append('request', JSON.stringify(requestData));
-    formData.append('profileImage', data.profileImage);
+    formData.append('request', new Blob([JSON.stringify(requestData)], { type: 'application/json' }));
+    if (data.profile_image) {
+      formData.append('profileImage', data.profile_image);
+    } else {
+      const defaultImageResponse = await fetch(defaultBackground);
+      const blob = await defaultImageResponse.blob();
+      const defaultBackgroundFile = new File([blob], 'defaultImage.webp');
+      formData.append('profileImage', defaultBackgroundFile);
+    }
     // console.log(formData.get('request'));
     // console.log(formData.get('profileImage'));
     createStudy(formData);
