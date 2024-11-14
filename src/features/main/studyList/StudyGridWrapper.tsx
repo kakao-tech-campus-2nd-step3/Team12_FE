397930e3a3f5c7c977f159b345ae17b1c4b646a4
cla6shade <cla6shade@gmail.com>
2024-11-15 01:18:56 +0900

    refactor: rename detailed study info type

--- src/features/main/studyList/StudyGridWrapper.tsx
+++ src/features/main/studyList/StudyGridWrapper.tsx
@@ -7,7 +7,7 @@ import { useInView } from 'react-intersection-observer';
 import StudyItemSkeleton from '@features/main/studyList/StudyItemSkeleton';
 import type {
   StudyFilter,
-  StudySearchInfo,
+  DetailedStudyInfo,
   StudySearchRequestQuery,
 } from '@/types/study';
 import { searchStudies } from '@/api/study';
@@ -80,7 +80,7 @@ function StudyGridWrapper({ studyFilter, searchKeyword }: StudyItemWrapperProps)
 }
 
 interface StudyItemContainerProps {
-  studyList: StudySearchInfo[];
+  studyList: DetailedStudyInfo[];
 }
 
 function StudyItemContainer({ studyList }: StudyItemContainerProps) {
