533640c90be65d3b6a5b83ce43166e308721af28
cla6shade <cla6shade@gmail.com>
2024-11-15 15:23:23 +0900

    feat: 로그인하지 않으면 스터디 정보 조회가 불가능하도록 변경

--- src/features/main/studyList/StudyItem.tsx
+++ src/features/main/studyList/StudyItem.tsx
@@ -5,9 +5,14 @@ import { useStudyItemStyles } from '@features/main/studyList/StudyList.styles';
 import Avatar from '@components/avatar';
 import { CSSObject, useTheme } from '@emotion/react';
 import UserIcon from '@assets/icons/user.svg?react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import routePaths from '@constants/routePaths';
+import { useContext, useState } from 'react';
+import ApplyStudyJoinModal from '@features/modal/invite/ApplyStudyJoinModal';
+import { MemberInfoContext } from '@providers/MemberInfoProvider';
+import toast from 'react-hot-toast';
 import { DetailedStudyInfo } from '@/types/study';
+import { getMyRole } from '@/api/study';
 
 interface StudyItemProps {
   study: DetailedStudyInfo;
@@ -19,7 +24,22 @@ function StudyItem(
   }: StudyItemProps,
 ) {
   const { containerStyle } = useStudyItemStyles();
+  const { isLoggedIn } = useContext(MemberInfoContext);
+  const [isJoinModalOpen, setIsJoinModalOpen] = useState(false);
+  const navigate = useNavigate();
   const theme = useTheme();
+  const handleStudyItemClick = async () => {
+    if (!isLoggedIn) {
+      toast.error('로그인이 필요한 작업입니다. 먼저 로그인해주세요!');
+      return;
+    }
+    const role = await getMyRole(study.id);
+    if (role === '미가입') {
+      setIsJoinModalOpen(true);
+      return;
+    }
+    navigate(routePaths.STUDY_INFO(study.id));
+  };
 
   const singleEllipsis: CSSObject = {
     textOverflow: 'ellipsis',
@@ -35,8 +55,15 @@ function StudyItem(
   };
 
   return (
-    <Link to={routePaths.STUDY_INFO(study.id)} css={{ textDecoration: 'none', color: 'black' }}>
-      <Container direction="column" height="100%" align="flex-start" padding="20px 20px 20px 26px" cssOverride={containerStyle}>
+    <>
+      <Container
+        direction="column"
+        height="100%"
+        align="flex-start"
+        padding="20px 20px 20px 26px"
+        cssOverride={containerStyle}
+        onClick={handleStudyItemClick}
+      >
         <Container justify="space-between" align="flex-start">
           <Container
             padding="8px 0"
@@ -78,7 +105,15 @@ function StudyItem(
           </Container>
         </Container>
       </Container>
-    </Link>
+      {isJoinModalOpen
+        && (
+        <ApplyStudyJoinModal
+          studyId={study.id}
+          open={isJoinModalOpen}
+          onClose={() => setIsJoinModalOpen(false)}
+        />
+        )}
+    </>
   );
 }
 
--- src/utils/network/index.ts
+++ src/utils/network/index.ts
@@ -1,4 +1,4 @@
-import axios, { HttpStatusCode, isAxiosError } from 'axios';
+import axios, { HttpStatusCode } from 'axios';
 import { tokenStorage } from '@/utils/storage';
 import { reIssueAccessToken } from '@/api/auth';
 
@@ -9,6 +9,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use((config) => {
   const nextConfig = config;
+
   const token = tokenStorage.get();
   if (!token) {
     return nextConfig;
@@ -17,22 +18,48 @@ axiosInstance.interceptors.request.use((config) => {
   return nextConfig;
 });
 
-axiosInstance.interceptors.response.use((config) => config, async (error) => {
-  if (!isAxiosError(error)) return error;
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (!axios.isAxiosError(error)) return Promise.reject(error);
 
-  const { config, response } = error;
-  if (!response || !config || response.status !== HttpStatusCode.Unauthorized
-  ) return Promise.reject(error);
-  try {
-    const reissueResponse = await reIssueAccessToken();
-    const auth = reissueResponse.headers.Authorization;
-    if (!auth || !auth.startsWith('Bearer ')) return await Promise.reject(error);
-    const accessToken: string = auth.split(' ')[1];
-    tokenStorage.set(accessToken);
-  } catch (e) {
-    tokenStorage.remove();
-  }
-  return axiosInstance(config);
-});
+    const originalRequest = error.config;
+    const { response } = error;
+
+    if (!response || !originalRequest || response.status !== HttpStatusCode.Unauthorized) {
+      return Promise.reject(error);
+    }
+
+    try {
+      if (originalRequest._retry) {
+        tokenStorage.remove();
+        window.alert('인증 유효기간이 만료되었습니다. 다시 로그인해주세요.');
+        window.location.reload();
+        return await Promise.reject(new Error('토큰 재발급 에러'));
+      }
+
+      originalRequest._retry = true;
+
+      const reissueResponse = await reIssueAccessToken();
+      const auth = reissueResponse.headers.Authorization;
+
+      if (!auth || !auth.startsWith('Bearer ')) {
+        tokenStorage.remove();
+        window.alert('인증 유효기간이 만료되었습니다. 다시 로그인해주세요.');
+        window.location.reload();
+        return await Promise.reject(new Error('토큰 재발급 에러'));
+      }
+
+      const accessToken: string = auth.split(' ')[1];
+      tokenStorage.set(accessToken);
+
+      originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+      return await axiosInstance(originalRequest);
+    } catch (e) {
+      tokenStorage.remove();
+      return Promise.reject(e);
+    }
+  },
+);
 
 export default axiosInstance;
